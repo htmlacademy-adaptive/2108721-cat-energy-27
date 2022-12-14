1e2b5bfb331fa37f9ba835fcc256a361fc869657
Roman Tsarew <romats64@gmail.com>
2022-12-14 21:59:16 +0100

    ДЗ№5, JS и корректировки

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+let navMain = document.querySelector('.nav');
+let navButton = document.querySelector('.header__button');
+
+navMain.classList.remove('nav--nojs');
+
+navButton.addEventListener('click', function () {
+  if (navMain.classList.contains ('nav--closed')) {
+    navMain.classList.remove ('nav--closed');
+    navMain.classList.add ('nav--opened');
+  } else {
+    navMain.classList.add('nav--closed');
+    navMain.classList.remove('nav--opened');
+  }
+});
