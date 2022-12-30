3962f71775aac5bb40f72b88657818fa2d7642c1
Roman Tsarew <romats64@gmail.com>
2022-12-30 14:19:47 +0100

    проблема с автоматизацией

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,31 @@ import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
+import csso from 'gulp-postcss';
+import rename from 'gulp-rename';
 import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
 
 // Styles
 
 export const styles = () => {
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
-    .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer()
+  return gulp.src('source/sass/style.scss', { sourcemaps: true }) //1. style.less найден файл
+    .pipe(plumber()) // 2. в файле обрабатываются ошибки
+    .pipe(sass().on('error', sass.logError)) // style.sass --> style.css
+    .pipe(postcss([ // style.css
+      autoprefixer(), // style.css -> превращается в style.css с префиксами
+      csso() // style.css[prefix] -> превращается в style.css[prefix, min], т.е минимизируется код, удаляя ненужные символы,знаки и прочее
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css')) // переименовать файл в min
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' })) // положить его в директорию
     .pipe(browser.stream());
+}
+
+// HTML
+export const html = () => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('build'));
 }
 
 // Server
@@ -23,7 +35,7 @@ export const styles = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -41,5 +53,5 @@ const watcher = () => {
 
 
 export default gulp.series(
-  styles, server, watcher
+  html, styles, server, watcher
 );
