021823b4a3c09cd820d48d7306e11ab9e5b0ec30
Roman Tsarew <romats64@gmail.com>
2023-01-08 10:25:40 +0100

    ДЗ№9

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,24 @@
-let navMain = document.querySelector('.nav');
-let navButton = document.querySelector('.header__button');
+let navMain = document.querySelector(".nav");
+let navButton = document.querySelector(".header__button");
 
-navMain.classList.remove('nav--nojs');
+navMain.classList.remove("nav--nojs");
 
-if (navMain.classList.contains('nav')) {
-  navMain.classList.add('nav--closed');
+if (navMain.classList.contains("nav")) {
+  navMain.classList.add("nav--closed");
 }
 
-
-navButton.addEventListener('click', function () {
-  if (navMain.classList.contains ('nav--closed')) {
-    navMain.classList.remove ('nav--closed');
-    navMain.classList.add ('nav--opened');
-  } else {
-    navMain.classList.add('nav--closed');
-    navMain.classList.remove('nav--opened');
+navButton.addEventListener(
+  "click",
+  function () {
+    if (navMain.classList.contains("nav--closed")) {
+      navMain.classList.remove("nav--closed");
+      navMain.classList.add("nav--opened");
+    } else {
+      navMain.classList.add("nav--closed");
+      navMain.classList.remove("nav--opened");
+    }
+  },
+  {
+    passive: true,
   }
-});
+);
