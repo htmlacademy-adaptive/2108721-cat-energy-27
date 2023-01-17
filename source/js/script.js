6fe64bed454fe75666309a0286ccc8ab09fea459
Roman Tsarew <romats64@gmail.com>
2023-01-17 10:50:19 +0100

    защита v3

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,21 @@
-let navMain = document.querySelector(".header-nav");
-let navButton = document.querySelector(".header__button");
+let navMain = document.querySelector('.header-nav');
+let navButton = document.querySelector('.header__button');
 
-navMain.classList.remove("header-nav--nojs");
+navMain.classList.remove('header-nav--nojs');
 
-if (navMain.classList.contains("header-nav")) {
-  navMain.classList.add("header-nav--closed");
+if (navMain.classList.contains('header-nav')) {
+  navMain.classList.add('header-nav--closed');
 }
 
 navButton.addEventListener(
-  "click",
+  'click',
   function () {
-    if (navMain.classList.contains("header-nav--closed")) {
-      navMain.classList.remove("header-nav--closed");
-      navMain.classList.add("header-nav--opened");
+    if (navMain.classList.contains('header-nav--closed')) {
+      navMain.classList.remove('header-nav--closed');
+      navMain.classList.add('header-nav--opened');
     } else {
-      navMain.classList.add("header-nav--closed");
-      navMain.classList.remove("header-nav--opened");
+      navMain.classList.add('header-nav--closed');
+      navMain.classList.remove('header-nav--opened');
     }
   },
   {
